d10948bc4da9bdb7568bee24bd70f93eadb3ad9e
alif <alifrazanit@gmail.com>
2022-07-17 17:26:14 +0700

    class, method, field

---
+++ src/PersonApp.java
@@ -0,0 +1,10 @@
+public class PersonApp {
+    public static void main(String[] args) {
+        var person1 = new Person();
+
+        person1.name = "ALIF";
+        person1.address = "Bogor";
+
+        System.out.println(person1.name);
+    }
+}
